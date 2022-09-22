0c95b0f1a4e36fe326a58e98c194de568e619a7a
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-22 11:56:12 +0900

    Feat - home route

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/auth";
 import { SignUp } from "../pages/auth/signUp";
+import { Home } from "../pages/home";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signUp" element={<SignUp />} />
         </Routes>
