8746e6bedf4292eaf7373093c0eb542af66f4001
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-22 10:52:08 +0900

    Feat - 라우터 설정

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "../pages/auth";
+import { SignUp } from "../pages/auth/signUp";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
