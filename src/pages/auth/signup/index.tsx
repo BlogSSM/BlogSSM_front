f372b15a829d6c166abf6b17557222ecd89bfca2
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-13 11:42:46 +0900

    Feat - 변수 설정

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,10 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Button } from "../../../styles/Button";
 import { Logo } from "../../../styles/Logo";
 import * as S from "../auth.styles";
 
 export const SignUp = () => {
   const signUpBtn = useRef<HTMLButtonElement>(null);
+  const [name, setName] = useState<string>();
+  const [id, setId] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [mail, setMail] = useState<string>();
+  const [link, setLink] = useState<string>();
 
   const signUpBtnClick = () => {
     if (signUpBtn.current !== null) {
@@ -17,14 +22,34 @@ export const SignUp = () => {
       <Logo width={"100"} src="../../assets/BlogSSM-logo-noback.png" />
       <S.LoginView>
         <S.MainText>회원가입</S.MainText>
-        <S.Input type="email" placeholder="성함을 입력해주세요." />
-        <S.Input type="text" placeholder="아이디를 입력해주세요." />
-        <S.Input type="password" placeholder="비밀번호를 입력해주세요." />
+        <S.Input
+          type="email"
+          placeholder="성함을 입력해주세요."
+          onChange={(e) => setName(e.target.value)}
+        />
+        <S.Input
+          type="text"
+          placeholder="아이디를 입력해주세요."
+          onChange={(e) => setId(e.target.value)}
+        />
+        <S.Input
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <S.Email>
-          <S.EmailInput type="text" placeholder="ex) 202110406" />
+          <S.EmailInput
+            type="text"
+            placeholder="ex) 202110406"
+            onChange={(e) => setMail(e.target.value)}
+          />
           <S.DefalutEmail>@bssm.hs.kr</S.DefalutEmail>
         </S.Email>
-        <S.Input type="text" placeholder="대표 블로그 링크를 입력해주세요." />
+        <S.Input
+          type="text"
+          placeholder="대표 블로그 링크를 입력해주세요."
+          onChange={(e) => setLink(e.target.value)}
+        />
         <Button
           height={60}
           ref={signUpBtn}
