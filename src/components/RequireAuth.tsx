34eb4e73120664e34fc0faba90af2a266f0e4d98
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-26 17:58:54 +0900

    Feat - token 유무 검사 컴포넌트

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,10 +1,8 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import { userState } from "../recoil";
 
 export const RequireAuth = ({ children }: { children?: React.ReactNode }) => {
-  const user = useRecoilValue(userState);
+  const user = localStorage.getItem("token");
   const location = useLocation();
   return user ? (
     <>{children}</>
