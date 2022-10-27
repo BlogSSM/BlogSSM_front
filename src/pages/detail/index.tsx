ece61d1c14a61531cba4d6231d2a6893d4c77792
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-27 10:37:05 +0900

    Feat - post detail

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
+import client from "../../util/client";
+import { Post } from "../../util/Post";
+import { Frame } from "./style.detail";
+
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Viewer } from "@toast-ui/react-editor";
 
 export const PostDetail = () => {
   const { post_id } = useParams();
-  return <div>hel</div>;
+  const [post, setPost] = useState<Post>();
+  console.log(post_id);
+
+  const getPostDetail = async () => {
+    const response = await client.get(`/community/get_contact?id=${post_id}`);
+    console.log(response.data);
+
+    setPost(response.data);
+  };
+
+  useMemo(() => getPostDetail(), []);
+  return (
+    <>
+      {post && (
+        <Frame>
+          <h3>{post.title}</h3>
+          <Viewer initialValue={post.contactId} />
+        </Frame>
+      )}
+    </>
+  );
 };
