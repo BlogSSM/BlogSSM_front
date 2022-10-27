b7de0a6de42711d201310b960049e231349b1bff
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-27 10:39:07 +0900

    Feat - Post interface

---
+++ src/util/Post.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  tid: number;
+  uid: number;
+  image: string;
+  title: string;
+  context: string;
+  userName: string;
+  date: string;
+  heart: number;
+  url: string[];
+  contactId: string;
+}
